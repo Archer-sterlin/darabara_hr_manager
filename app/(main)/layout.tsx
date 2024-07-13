fae3db8a6e5ce5cff90fb2a7b35fdcf3844bd743
Python Web <pyweb@e4email.net>
2024-07-13 09:39:51 +0100

    new pages

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -41,8 +41,8 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
         <>
             <Navbar />
 
-            <div className="flex h-screen">
-                <div className="hidden md:block h-full w-[300px]">
+            <div className="flex">
+                <div className="hidden md:block h-[100vh] w-[300px]">
                     <Sidebar />
                 </div>
                 <div className="p-5 w-full md:max-w-[1140px] overflow-auto">
