4ec6a343c9719a0bc4a45c078d51e792d708b2b1
Python Web <pyweb@e4email.net>
2024-07-14 18:19:01 +0100

    fix http request error

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -3,29 +3,22 @@
 import * as z from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 import { useEffect, useState } from 'react';
 import { toast } from '../ui/use-toast';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 const formSchema = z.object({
   username: z.string().min(3, {
-    message: 'username is required.',
+    message: 'Username is required.',
   }),
   password: z.string().min(3, {
-    message: 'password is required',
+    message: 'Password is required',
   }),
 });
 
@@ -55,34 +48,27 @@ const LoginForm = () => {
   const handleSubmit = async (form_data: z.infer<typeof formSchema>) => {
     try {
       const { data } = await axios.post('https://chile64.pythonanywhere.com/api/v1/auth/login/', form_data);
- 
+      if (data.token.access) {
+        localStorage.setItem('access', data.token.access);
+        localStorage.setItem('refresh', data.token.refresh);
+        localStorage.setItem('profile', JSON.stringify(data.data));
 
-        if (data.token.access){
-          localStorage.setItem('access', data.token.access);
-          localStorage.setItem('refresh', data.token.refresh);
-          localStorage.setItem('profile', JSON.stringify(data.data));
-
-          const decoded: any = jwt_decode(data.token.access);
-          setUser(decoded);
-          router.push(`/employees/me`);
-        }
-        else {
-          toast({
-            title: 'Error',
-            description: `Failed to update`,
-          });
-        }
-    }
-    
-     catch (error) {
+        const decoded: any = jwt_decode(data.token.access);
+        setUser(decoded);
+        router.push(`/employees/me`);
+      } else {
+        toast({
+          title: 'Error',
+          description: `Failed to update`,
+        });
+      }
+    } catch (error) {
       toast({
         title: 'Error',
         description: `${error}`,
       });
     }
   };
-};
-
 
   return (
     <Card>
@@ -90,21 +76,21 @@ const LoginForm = () => {
         <CardTitle>Login</CardTitle>
         <CardDescription>Please enter your credentials</CardDescription>
       </CardHeader>
-      <CardContent className='space-y-6'>
+      <CardContent className="space-y-6">
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-6'>
+          <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6">
             <FormField
               control={form.control}
-              name='username'
+              name="username"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
-                    username
+                  <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
+                    Username
                   </FormLabel>
                   <FormControl>
                     <Input
-                      className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
-                      placeholder='Enter username'
+                      className="bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0"
+                      placeholder="Enter username"
                       {...field}
                     />
                   </FormControl>
@@ -114,17 +100,17 @@ const LoginForm = () => {
             />
             <FormField
               control={form.control}
-              name='password'
+              name="password"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
-                    password
+                  <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
+                    Password
                   </FormLabel>
                   <FormControl>
                     <Input
-                      type='password'
-                      className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
-                      placeholder=' Enter password'
+                      type="password"
+                      className="bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0"
+                      placeholder=" Enter password"
                       {...field}
                     />
                   </FormControl>
@@ -132,7 +118,7 @@ const LoginForm = () => {
                 </FormItem>
               )}
             />
-            <Button type='submit' className='w-full dark:bg-slate-800 dark:text-white'>
+            <Button type="submit" className="w-full dark:bg-slate-800 dark:text-white">
               Login
             </Button>
           </form>
